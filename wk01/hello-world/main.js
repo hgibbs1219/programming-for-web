afc3730a0f252e60e037c1719fccf66201ac543a
hgibbs1219 <hgibbs@mcad.edu>
2025-01-22 20:37:23 -0600

    Added script file

---
+++ wk01/hello-world/main.js
@@ -0,0 +1,9 @@
+
+
+// Exercise 1.1 | Logging Hellow World
+console.log("Hello" + " " + "World")
+
+//Exercise 1.1 pt 2 | Prompting for user's name
+
+let myName = window.prompt("What is your name?")
+console.log("Hello" + " " + myName)
