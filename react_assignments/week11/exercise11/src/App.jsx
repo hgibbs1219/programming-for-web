98135422e3e5946fea4b55b6314c1e6889428db5
hgibbs1219 <hgibbs@mcad.edu>
2026-04-09 20:24:23 -0500

    Update App.jsx

--- react_assignments/week11/exercise11/src/App.jsx
+++ react_assignments/week11/exercise11/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import SimpleCard from './SimpleCard'
+import SimpleCard from './SampleCard'
 
 
 function App() {
